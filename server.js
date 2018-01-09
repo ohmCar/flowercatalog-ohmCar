783ae812c09f10fe519fd27efc0acd2244532a35
omkar mote <omkarmote210@gmail.com>
2018-01-09 18:00:18 +0530

    added a feature which redirects to guestbook after login only if the user is registered

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const WebApp = require('./webapp.js');
 const commentsFile=require(pathOfCommentsFile);
 
 let app = WebApp.create();
+let registeredUsers=['omkar'];
 
 const setHeader=function(req,res){
   let file=".."+req.url;
@@ -38,6 +39,29 @@ const serveFile=function(req,res){
 }
 
 app.use(serveFile);
+
+app.get('/login',(req,res)=>{
+  res.setHeader('Content-type','text/html');
+  if(req.cookies.logInFailed) res.write('<p>logIn Failed</p>');
+  res.write(
+    `<form method="post">
+    Username: <input type="text" name="userName">
+    <input type="submit">
+    </form>`
+  );
+  res.end();
+});
+
+app.post('/login',(req,res)=>{
+  let user = registeredUsers.includes(req.body.userName);
+  if(!user){
+    res.setHeader('Set-Cookie',`logInFailed=true`);
+    res.redirect('/login');
+    return;
+  }
+  res.setHeader('Set-Cookie',`logInFailed=false`);
+  res.redirect('/guestBook.html');
+});
 
 const PORT=8001;
 const server=http.createServer(app);
